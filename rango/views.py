db01018aeccede87a0b559960477c7c90751d202
didi04 <2091499s@student.gla.ac.uk>
2018-01-21 21:42:15 +0000

    4.4 Exercise

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ def index(request):
 	context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
 	return render(request, 'rango/index.html', context=context_dict)
 
-# An about view with a link to the index page
+# An about view with a link to the index page and a file input
 def about(request):
-	return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
+    context_dict = {'authormessage': "This tutorial has been put together by Dilyana Savcheva"}
+    return render(request, 'rango/about.html', context=context_dict)
