c84161877884c52f50e34e1aaa9ba85cb7934862
didi04 <2091499s@student.gla.ac.uk>
2018-01-21 18:19:34 +0000

    3.6 Exercises

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse 
 
+# An inital view (index view) with a link to the about page
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+# An about view with a link to the index page
+def about(request):
+	return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
